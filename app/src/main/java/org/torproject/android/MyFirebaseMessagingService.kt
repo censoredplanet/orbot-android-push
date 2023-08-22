d87edaecb5240bd0a7b41378c601e3cb7790aa8c
Ye Shu <hello@shuye.dev>
2023-08-21 23:09:23 -0400

    push: Add FCM Service

---
+++ app/src/main/java/org/torproject/android/MyFirebaseMessagingService.kt
@@ -0,0 +1,86 @@
+package org.torproject.android
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+import okhttp3.*
+import java.io.IOException
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    /**
+     * Called if the FCM registration token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the
+     * FCM registration token is initially generated so this is where you would retrieve the token.
+     */
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // FCM registration token to your app server.
+        sendRegistrationToServer(token)
+    }
+
+    private fun sendRegistrationToServer(token: String) {
+        // this is the computer's address in Android Virtual Machine
+        val url = "http://10.0.2.2:8888"
+        val client = OkHttpClient()
+        val requestBody = FormBody.Builder()
+            .add("token", token)
+            .add("country", "us")
+            .build()
+
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e(TAG, "Failed to send token to server: ${e.message}")
+                // TODO: handle the error, e.g. display user notification?
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                Log.d(TAG, "Token sent to server successfully")
+            }
+        })
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: ${remoteMessage.from}")
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+
+            // TODO: save the bridges in settings
+
+//            if (/* Check if data needs to be processed by long running job */ true) {
+//                // For long-running tasks (10 seconds or more) use WorkManager.
+//                scheduleJob()
+//            } else {
+//                // Handle message within 10 seconds
+//                handleNow()
+//            }
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+    companion object {
+        private const val TAG = "push-notification"
+    }
+}
